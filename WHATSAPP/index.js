f7c666137c3f523f1a173eb813b297d11d4d2953
Uriel <animaster771@gmail.com>
2026-03-23 22:42:15 -0300

    Update index.js

--- WHATSAPP/index.js
+++ WHATSAPP/index.js
@@ -10,7 +10,6 @@ const client = new Client({
   authStrategy: new LocalAuth(),
   puppeteer: {
     headless: true,
-    executablePath: '/usr/bin/chromium',
     args: [
       "--no-sandbox",
       "--disable-setuid-sandbox",

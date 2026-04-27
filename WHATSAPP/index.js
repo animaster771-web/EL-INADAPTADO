f8c46a8dc5f0e1a2793d7d98085cd6c9dfa7618b
Uriel <animaster771@gmail.com>
2026-04-27 16:44:02 -0300

    Update index.js

--- WHATSAPP/index.js
+++ WHATSAPP/index.js
@@ -7,7 +7,15 @@ const { createCanvas, loadImage } = require("canvas");
 const client = new Client({
   authStrategy: new LocalAuth(),
   puppeteer: {
-    args: ['--no-sandbox', '--disable-setuid-sandbox']
+    headless: true,
+    args: [
+      '--no-sandbox',
+      '--disable-setuid-sandbox',
+      '--disable-dev-shm-usage',
+      '--disable-accelerated-2d-canvas',
+      '--no-zygote',
+      '--single-process'
+    ]
   }
 });
 

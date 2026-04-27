2cdf280ca8154e2fb98068e93279aa4174a92e17
Uriel <animaster771@gmail.com>
2026-04-27 17:21:50 -0300

    Update index.js

--- WHATSAPP/index.js
+++ WHATSAPP/index.js
@@ -12,9 +12,7 @@ const client = new Client({
       '--no-sandbox',
       '--disable-setuid-sandbox',
       '--disable-dev-shm-usage',
-      '--disable-accelerated-2d-canvas',
-      '--no-zygote',
-      '--single-process'
+      '--disable-gpu'
     ]
   }
 });
